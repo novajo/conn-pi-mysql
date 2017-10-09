dcf6df17662ee06aafa2cd84bbf01c388f39a41e
novajo <novajo@gmail.com>
2017-10-09 20:20:54 +0800

    first commit

--- pimysql.py
+++ pimysql.py
@@ -4,8 +4,8 @@ import pymysql
 
 def select():
     conn = pymysql.connect(host='127.0.0.1',
-                           user='nova', passwd='password',
-                           db='wechat',
+                           user='nova', passwd='@@',
+                           db='@@@',
                            port=3306,
                            charset='utf8')
     cur = conn.cursor()
@@ -17,9 +17,9 @@ def select():
 
 
 def insert(fromUser, CreateTime, msgType, QR_ID):
-    conn = pymysql.connect(host='w3c.poissonodds.win',
-                           user='nova', passwd='password',
-                           db='wechat',
+    conn = pymysql.connect(host='127.0.0.1',
+                           user='nova', passwd='@@',
+                           db='@@@',
                            port=3306,
                            charset='utf8')
     cur = conn.cursor()
